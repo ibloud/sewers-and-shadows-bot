88f360d800c09e6ebd917a4191ea344107bff641
Dominique Devereaux <90852374+ibloud@users.noreply.github.com>
2026-07-18 14:22:24 -0500

    Fix import path for float-chess-engine.js

--- bot/src/pennywiseAI.js
+++ bot/src/pennywiseAI.js
@@ -24,7 +24,7 @@ const {
   pseudoLegalMoves,
   moveIsSafe,
   isColorInCheck,
-} = require('../../float-chess-engine.js');
+} = require('../float-chess-engine.js');
 
 const PIECE_VALUE = { P: 1, N: 3, B: 3, R: 5, Q: 9, K: 0 };
 
